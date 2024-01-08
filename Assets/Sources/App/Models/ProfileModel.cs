8dd76324da08241bb824338cb08f67fc1c654f77
DESKTOP-RPC9O4H\Carhvax <cjjoker@yandex.ru>
2024-01-08 15:48:20 +0200

    - added experience progression;

--- Assets/Sources/App/Models/ProfileModel.cs
+++ Assets/Sources/App/Models/ProfileModel.cs
@@ -2,6 +2,8 @@ public class ProfileModel : IAppModel {
     
     public IObservableValue<int> BestScore { get; } = new ObservableValue<int>(0);
     public IObservableValue<int> Score { get; } = new ObservableValue<int>(0);
+    
+    public IObservableValue<int> Experience { get; } = new ObservableValue<int>(0);
     public IObservableValue<int> Level { get; } = new ObservableValue<int>(1);
     public IObservableValue<float> Progress { get; } = new ObservableValue<float>(0);
     public IObservableValue<bool> Win { get; } = new ObservableValue<bool>(false);
@@ -11,13 +13,13 @@ public class ProfileModel : IAppModel {
         get => new ProfileState() {
             level = Level.Value,
             bestScore = BestScore.Value,
-            score = Score.Value,
+            experience = Experience.Value,
             progress = Progress.Value,
         };
         set {
             Level.Value = value.level;
             BestScore.Value = value.bestScore;
-            Score.Value = value.score;
+            Experience.Value = value.experience;
             Progress.Value = value.progress;
         }
     }
@@ -34,6 +36,17 @@ public class ProfileModel : IAppModel {
     public void AffectScore() {
         if (Score.Value > BestScore.Value)
             BestScore.Value = Score.Value;
+        
+        var value = Experience.Value;
+        value += Score.Value;
+
+        while(value / 100f >= 1) {
+            Level.Value++;
+            value -= 100;
+        }
+
+        Experience.Value = value;
+        Progress.Value = value / 100f;
     }
     
     public void WinGame() {
@@ -44,7 +57,7 @@ public class ProfileModel : IAppModel {
 
 public struct ProfileState {
     public int bestScore;
-    public int score;
+    public int experience;
     public int level;
     public float progress;
 }
